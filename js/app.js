18adb2545ab1ff78be53ee7d2acf89a21ae8146b
ijustin125i <61374722+ijustin125i@users.noreply.github.com>
2022-06-30 23:24:51 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+const burger = document.getElementById("hamburger");
+
+burger.addEventListener("click", () => {
+  burger.textContent = "Home About";
+  burger.style.color = 'var(--clr-primary-5)';
+});
+
+
